d98f952c81a8887374a824b3b1617481a3df397f
Emma Goldberg <eeg@umn.edu>
2014-10-17 15:32:30 -0500

    relative redirect

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf genindex.html objects.inv search.html searchindex.js _sources/
 echo "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 3.2//EN'>\n
-<html>\n <head> <meta http-equiv='refresh' content='0; url=http://www.tc.umn.edu/~eeg/home.html'> <title> </title> </head> <body> redirecting... </body>\n </html>" > index.html
+<html>\n <head> <meta http-equiv='refresh' content='0; url='home.html'> <title> </title> </head> <body> redirecting... </body>\n </html>" > index.html
