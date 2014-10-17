0a9fdf676b051321385964714121b0c56479928f
Emma Goldberg <eeg@umn.edu>
2014-10-17 15:38:40 -0500

    redirect fix

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 rm -rf genindex.html objects.inv search.html searchindex.js _sources/
-echo "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 3.2//EN'>\n
-<html>\n <head> <meta http-equiv='refresh' content='0; url='home.html'> <title> </title> </head> <body> redirecting... </body>\n </html>" > index.html
+
+echo "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 3.2//EN'>
+<html>
+  <head>
+    <meta http-equiv='refresh' content='0; url=home.html'>
+    <title> </title>
+  </head>
+  <body>
+    redirecting...
+  </body>
+</html>" > index.html
