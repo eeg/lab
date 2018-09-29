f323f9bf900cec7a5cd346e624f19c8573d318d1
Emma Goldberg <emma@gila>
2018-09-28 21:16:18 -0500

    small

--- copy.sh
+++ copy.sh
@@ -5,7 +5,7 @@ rm -rf _images/ _static/*.* _static/reprints/ *.html
 cp -r  ../_build/html/* .
 cp     ../_build/html/_static/goldberg_cv.pdf _static/
 
-rm -rf genindex.html objects.inv search.html searchindex.js _sources/
+rm -rf genindex.html objects.inv search.html searchindex.js _sources/ _static/logos.xcf
 
 echo "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 3.2//EN'>
 <html>
